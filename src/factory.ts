b5850fecaf764c5dc158a140bcc5359f565762e8
Gal Schlezinger <gal@spitfire.co.il>
2020-01-22 13:19:51 +0200

    add docs

--- src/factory.ts
+++ src/factory.ts
@@ -1,9 +1,25 @@
 import { inspect } from 'util';
-export function factory<T>(
-  defaults?: Partial<T>
-): (overrides?: Partial<T>) => T {
-  return overrides => {
-    const data = { ...defaults, ...overrides };
+
+/**
+ * A factory that will throw errors when accessing
+ * an unsetted value
+ */
+export type Factory<T> = (
+  /**
+   * Attributes to set for the instance.
+   * These will extend and override the attributes provided to the `defaults` when the factory was created
+   */
+  attributes?: Partial<T>
+) => T;
+
+/**
+ * Creates a strict factory
+ *
+ * @param defaults the default values that would appear on all instances
+ */
+export function factory<T>(defaults?: Partial<T>): Factory<T> {
+  return attributes => {
+    const data = { ...defaults, ...attributes };
 
     return new Proxy(data, {
       has(target: Partial<T>, p: keyof T) {
